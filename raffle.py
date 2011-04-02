dd6bbaaed39622ee9572b7437e34119ffaa0f17e
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-02 14:11:00 -0400

    Commented things.

--- raffle.py
+++ raffle.py
@@ -6,17 +6,23 @@ app = Flask(__name__)
 
 DATABASE = 'raffle.db'
 
+
 @app.before_request
 def before_request():
+    """Connects to the database on each request"""
     g.db = sqlite3.connect(DATABASE)
-    
+
+
 @app.after_request
 def after_request(response):
+    """Close the db connection."""
     g.db.close()
     return response
 
+
 @app.route('/')
 def pick_user():
+    """Picks a random participant that hasn't yet won a prize."""
     users = g.db.execute('SELECT id, name FROM participants WHERE id NOT IN (SELECT participant_id FROM winners)').fetchall()
     user = random.choice(users)
     return render_template('winner.html', id=user[0], name=user[1])
@@ -24,27 +30,32 @@ def pick_user():
 
 @app.route('/add_winner', methods=('POST',))
 def add_winner():
+    """Records the prize that a participant won."""
     user_id = int(request.form['user_id'])
     prize = request.form['prize']
     with g.db:
-        g.db.execute("INSERT INTO winners(participant_id, prize_name) values (?, ?)", [user_id, prize])
+        g.db.execute("INSERT INTO winners(participant_id, prize_name) values (?, ?)", (user_id, prize))
     
     username = g.db.execute('SELECT name FROM participants WHERE id=?', (user_id,)).fetchone()
     return render_template('winner_added.html', name=username[0], prize=prize)
-    
+
+
 @app.route('/winners')
 def winners():
+    """Lists all raffle winners, and what they won."""
     winners = g.db.execute("SELECT participants.name, winners.prize_name FROM participants INNER JOIN winners ON participants.id = winners.participant_id").fetchall()
     return render_template('winners_list.html', winners=winners)
 
+
 def build_db():
-    DB = sqlite3.connect('raffle.db')
+    """Builds the database if necessary."""
+    db = sqlite3.connect('raffle.db')
     try:
-        DB.execute('SELECT 1 FROM participants')
+        db.execute('SELECT 1 FROM participants')
     except sqlite3.OperationalError:
-        with DB:
-            DB.execute('CREATE TABLE participants (id INTEGER PRIMARY KEY, name text)')
-            DB.execute("""CREATE TABLE winners (
+        with db:
+            db.execute('CREATE TABLE participants (id INTEGER PRIMARY KEY, name text)')
+            db.execute("""CREATE TABLE winners (
                     participant_id INTEGER, 
                     prize_name TEXT,
                     FOREIGN KEY(participant_id) REFERENCES participants(id)
@@ -56,8 +67,10 @@ def build_db():
                     users.add(line.strip())
                 
                 for user in users:
-                    DB.execute('INSERT INTO participants(name) values (?)', (user,))
-        DB.close()
+                    db.execute('INSERT INTO participants(name) values (?)', (user,))
+        db.close()
+
+
 if __name__ == '__main__':
     build_db()
     app.debug = True
