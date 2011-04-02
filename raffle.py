8d3fdc5b13779d36d8192e0a1f803645a9802bd7
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-02 14:19:25 -0400

    pep8.

--- raffle.py
+++ raffle.py
@@ -23,10 +23,12 @@ def after_request(response):
 @app.route('/')
 def pick_user():
     """Picks a random participant that hasn't yet won a prize."""
-    users = g.db.execute('SELECT id, name FROM participants WHERE id NOT IN (SELECT participant_id FROM winners)').fetchall()
+    users = g.db.execute("""SELECT id, name FROM participants WHERE id NOT IN
+                        (SELECT participant_id FROM winners)""").fetchall()
+
     user = random.choice(users)
     return render_template('winner.html', id=user[0], name=user[1])
-    
+
 
 @app.route('/add_winner', methods=('POST',))
 def add_winner():
@@ -34,16 +36,24 @@ def add_winner():
     user_id = int(request.form['user_id'])
     prize = request.form['prize']
     with g.db:
-        g.db.execute('INSERT INTO winners(participant_id, prize_name) values (?, ?)', (user_id, prize))
-    
-    username = g.db.execute('SELECT name FROM participants WHERE id=?', (user_id,)).fetchone()
+        g.db.execute("""INSERT INTO winners(participant_id, prize_name)
+            VALUES (?, ?)""", (user_id, prize))
+
+    username = g.db.execute("""SELECT name
+        FROM participants
+        WHERE id=?""", (user_id,)).fetchone()
+
     return render_template('winner_added.html', name=username[0], prize=prize)
 
 
 @app.route('/winners')
 def winners():
     """Lists all raffle winners, and what they won."""
-    winners = g.db.execute('SELECT participants.name, winners.prize_name FROM participants INNER JOIN winners ON participants.id = winners.participant_id').fetchall()
+    winners = g.db.execute("""SELECT participants.name, winners.prize_name
+        FROM participants
+        INNER JOIN winners
+        ON participants.id = winners.participant_id""").fetchall()
+
     return render_template('winners_list.html', winners=winners)
 
 
@@ -54,20 +64,25 @@ def build_db():
         db.execute('SELECT 1 FROM participants')
     except sqlite3.OperationalError:
         with db:
-            db.execute('CREATE TABLE participants (id INTEGER PRIMARY KEY, name text)')
+            db.execute("""CREATE TABLE participants (
+                id INTEGER PRIMARY KEY, name text
+            )""")
+
             db.execute("""CREATE TABLE winners (
-                    participant_id INTEGER, 
+                    participant_id INTEGER,
                     prize_name TEXT,
                     FOREIGN KEY(participant_id) REFERENCES participants(id)
                 )""")
-            
+
             with open('data.txt', 'r') as data:
                 users = set()
                 for line in data.xreadlines():
                     users.add(line.strip())
-                
+
                 for user in users:
-                    db.execute('INSERT INTO participants(name) values (?)', (user,))
+                    db.execute("""INSERT INTO participants(name)
+                        VALUES (?)""", (user,))
+
         db.close()
 
 
